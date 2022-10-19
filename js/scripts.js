416641c67f89abbe5c40443aeeb4a1f9c897215c
LukeCallahan <lukemcal@gmail.com>
2022-10-19 10:48:46 -0700

    add core form functionality

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,30 @@ window.onload = function(){
 //form submission
 
 let form = document.querySelector("form");
-form.onsubmit = function(event) {
+form.onsubmit = function(e) {
+//get the value from each form input.
+const person1Input = document.getElementById("person1Input").value;
+const person2Input = document.getElementById("person2Input").value;
+const animalInput = document.getElementById("animalInput").value;
+const exclamationInput = document.getElementById("exclamationInput").value;
+const verbInput = document.getElementById("verbInput").value;
+const nounInput = document.getElementById("nounInput").value;
+
+
 //print values to the story area;
-document.querySelector("span#person1a").innerText = "person1";
-document.querySelector("span#person1b").innerText = "person1";
-document.querySelector("span#person1c").innerText = "person1";
-document.querySelector("span#person2a").innerText = "person 2";
-document.querySelector("span#person2b").innerText = "person 2";
-document.querySelector("span#animal").innerText = "an animal";
-document.querySelector("span#verb").innerText = "verb";
-document.querySelector("span#noun").innerText = "noun";
-document.querySelector("span#exclamation").innerText = "exclamation";
+document.querySelector("span#person1a").innerText = person1Input;
+document.querySelector("span#person1b").innerText = person1Input;
+document.querySelector("span#person1c").innerText = person1Input;
+document.querySelector("span#person2a").innerText = person2Input;
+document.querySelector("span#person2b").innerText = person2Input;
+document.querySelector("span#animal").innerText = animalInput;
+document.querySelector("span#verb").innerText = verbInput;
+document.querySelector("span#noun").innerText = nounInput;
+document.querySelector("span#exclamation").innerText = exclamationInput;
 
-//remove the class attribute
+// //remove the class attribute
 document.querySelector("div#story").removeAttribute("class");
-
-};
+//prevent default refresh action for the submit event
+e.preventDefault();
+  };
 };
